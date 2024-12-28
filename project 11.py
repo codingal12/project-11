fda869f75b074438228dc19dc4b033b609b7a543
codingal12 <sudhajoshi093@gmail.com>
2024-12-28 20:59:04 +0530

    Add files via upload

---
+++ project 11.py
@@ -0,0 +1,7 @@
+###this a project
+num1=int(input("enter the 1 st number"))
+num2=int(input("enter the 2 nd number"))
+num3=int(input("enter the 3 rd number"))
+print(num1==num3)
+print(num2==num1)
+print(num3==num2)
